c639edf1d1d643934e16e5f7ef4353a78d395c77
pretowag <143122649+pretowag@users.noreply.github.com>
2024-09-25 08:50:18 -0300

    rwerw

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ function areaquadrado (lado){
    return lado**2;
 }
 console.log(areaquadrado(6));
+let 1= prompt(1?)
+let 2= prompt(2?)
+let 3= prompt(3?)
+
+let media=function(1,2,3){
+return (1,2,3)/3;
+}
+console.log ( media(3,7,9))
