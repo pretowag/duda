5dc175deac1567752192ed74c4448acc46ffced5
pretowag <143122649+pretowag@users.noreply.github.com>
2024-10-02 07:40:24 -0300

    wrwrwr

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-function soma (numero1,numero2){
-   return numero1+numero2 
-}
-console.log(soma(9,7));
+// function soma (numero1,numero2){
+//    return numero1+numero2 
+// }
+// console.log(soma(9,7));
 
-function areaquadrado (lado){
-   return lado**2;
-}
-console.log(areaquadrado(6));
-let 1= prompt(1?)
-let 2= prompt(2?)
-let 3= prompt(3?)
+// function areaquadrado (lado){
+//    return lado**2;
+// }
+// console.log(areaquadrado(6));
+// let 1= prompt(1?)
+// let 2= prompt(2?)
+// let 3= prompt(3?)
 
-let media=function(1,2,3){
-return (1,2,3)/3;
-}
-console.log ( media(3,7,9))
+// let media=function(1,2,3){
+// return (1,2,3)/3;
+// }
+// console.log ( media(3,7,9))
+
+let estados=['DF','GO','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','SC']
