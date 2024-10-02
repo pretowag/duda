183d563efe726773cd8e575f300e6ff288d2e94f
pretowag <143122649+pretowag@users.noreply.github.com>
2024-10-02 07:48:09 -0300

    rwrwqr

--- main.js
+++ main.js
@@ -16,4 +16,4 @@
 // }
 // console.log ( media(3,7,9))
 
-let estados=['DF','GO','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','SC']
+let estados=['DF','GO','AL','BA','CE','MA','PB','PE','PI','RN','SE','AC','AP','AM','PA','RO','RR','TO','ES','MG','RJ','SP','PR','SC','RS','MS','MG']
